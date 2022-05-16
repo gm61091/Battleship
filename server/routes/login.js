79c2f05cc25d9dad8c54bf362a58bd0e98d684a0
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2022-05-16 14:51:03 -0500

    setup login post route

---
+++ server/routes/login.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const jwt = require("jwt-simple");
+const secretObj = require("../secrets");
+const passport = require("passport");
+
+router.use(passport.initialize());
+
+require("../auth/passAuth");
+
+const requireLogin = passport.authenticate("local", { session: false });
+
+const generateToken = userRecord => {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: userRecord.id, iat: timestamp }, secretObj.secret)
+}
+
+router.post("/login", requireLogin, (req, res) => {
+    res.json({
+        token: generateToken(req.user),
+        wins: req.user.wins,
+        losses: req.user.losses
+    })
+})
+
+module.exports = router;
