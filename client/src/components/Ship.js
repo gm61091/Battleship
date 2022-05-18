b75afd94191a9617a859d0f19ec9a82cbad66242
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2022-05-18 11:55:38 -0500

    added click event for ship to update ship length in global state

--- client/src/components/Ship.js
+++ client/src/components/Ship.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./GridSquare.css";
+import modifyShipLength from "../actions/modifyShipLength"
+import { useDispatch } from "react-redux";
 
 const Ship = (props) => {
 
-    let shipArray =new Array(props.shipLength).fill(0)
+    let shipArray = new Array(props.shipLength).fill(0);
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+      dispatch(modifyShipLength(props.shipLength));
+    }
 
     return (
-      <div className="ship">
+      <div className="ship" onClick={handleClick}>
         {shipArray.map(() => <div className="boat-square"></div>)}
       </div>
     )  
