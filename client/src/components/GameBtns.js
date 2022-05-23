a44899496dd7b067f19320336fb4c6c785783829
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2022-05-23 13:58:14 -0500

    randomly selects who goes first. tested and works

--- client/src/components/GameBtns.js
+++ client/src/components/GameBtns.js
@@ -2,19 +2,30 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "react-bootstrap/Button";
 
-
 import { modifyShipOrientation, resetGameBoard, resetGame } from "../actions/gameStartActions";
-import { startGame } from "../actions/gamePlayActions";
+import { startGame, nextComputerMove, setComputerTurn, computerMessage } from "../actions/gamePlayActions";
 
 const GameBtns = () => {
 
     const dispatch = useDispatch();
     const { shipOrientation, shipLengths } = useSelector(state => state.gameStart);
-    const { gameStarted, computerShipCoordinates, message, shipCoordinates, gameOver } = useSelector(state => state.gamePlay);
+    const { gameStarted, gameOver } = useSelector(state => state.gamePlay);
+
+    const handleStartGame = () => {
+        dispatch(startGame());
+        const randomNumber = Math.floor(Math.random() * 2);
+        if (randomNumber === 0) {
+            dispatch(setComputerTurn());
+            dispatch(computerMessage("Computer goes first"));
+            setTimeout(() => {
+                dispatch(nextComputerMove());
+            }, 1 * 1000)
+        }
+    }
 
     return (
         <div className="game-btns mt-3">
-            {!gameStarted &&  
+            {!gameStarted &&
                 <>
                     <Button variant="primary" onClick={() => dispatch(modifyShipOrientation())}>
                         {shipOrientation === "vertical" ? "Horizontal" : "Vertical"}
@@ -22,19 +33,14 @@ const GameBtns = () => {
                     <Button variant="primary mx-3" onClick={() => dispatch(resetGameBoard())}>
                         Reset Board
                     </Button>
-                </> 
+                </>
             }
-            {!shipLengths.length && !gameStarted && 
-                <Button variant="primary mx-3" onClick={() => dispatch(startGame())}>
+            {!shipLengths.length && !gameStarted &&
+                <Button variant="primary mx-3" onClick={handleStartGame}>
                     Start Game
                 </Button>
             }
-            {/* {gameStarted && 
-                <Button variant="primary mx-3" onClick={() => dispatch(nextComputerMove())}>
-                    Next Move
-                </Button>
-            } */}
-            {gameOver && 
+            {gameOver &&
                 <Button variant="primary mx-3" onClick={() => dispatch(resetGame())}>
                     Reset Game
                 </Button>
