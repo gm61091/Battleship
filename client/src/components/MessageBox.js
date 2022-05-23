e283721ea0a102eb85088f7aa1e153975fc9d0da
gary merriman <g.merriman691@gmail.com>
2022-05-23 14:19:00 -0400

    save

--- client/src/components/MessageBox.js
+++ client/src/components/MessageBox.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { updateUserLossesInDatabase, updateUserWinsInDatabase } from "../utils/updateUserRecord";
 import { updateWins, updateLosses } from "../actions/userActions";
 import { setMessage } from "../actions/gamePlayActions";
+import GameMessage from "./GameMessage"
 
 const MessageBox = () => {
 
@@ -11,23 +12,25 @@ const MessageBox = () => {
     const { gameStarted, message, shipCoordinates, gameOver } = useSelector(state => state.gamePlay);
     const { wins, losses, email } = useSelector(state => state.user);
 
-    useEffect(() => {
-        if (gameOver && shipCoordinates.length) {
-            setMessage("You win!");
-            updateUserWinsInDatabase(email, wins + 1);
-            dispatch(updateWins(wins + 1));
-        } else if (gameOver) {
-            setMessage("You lose!");
-            updateUserLossesInDatabase(email, losses + 1);
-            dispatch(updateLosses(losses + 1));
-        }
-    }, [gameOver])
+    // useEffect(() => {
+    //     if (gameOver && shipCoordinates.length) {
+    //         setMessage("You win!");
+    //         updateUserWinsInDatabase(email, wins + 1);
+    //         dispatch(updateWins(wins + 1));
+    //     } else if (gameOver) {
+    //         setMessage("You lose!");
+    //         updateUserLossesInDatabase(email, losses + 1);
+    //         dispatch(updateLosses(losses + 1));
+    //     }
+    // }, [gameOver])
 
     return (
         <div className="message-box mx-3">
-            {gameStarted && message ? message : ""}
+            {(!gameStarted || gameOver) && message}
+            {(!gameOver && gameStarted) && <GameMessage />}
         </div>
     )
 }
+
 
 export default MessageBox;
