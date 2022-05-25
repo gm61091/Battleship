db3714992d1bd0f21dc5bf0df4cb70fab69d8c3a
Dez <warningdez1@gmail.com>
2022-05-25 10:55:59 -0400

    updated footer css

--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -5,7 +5,11 @@ const Footer = () => {
     return (
         <>
         <footer className="footer-txt">
-            <p>Copyright &copy; <a href="https://github.com/jamescr757" target="_blank">James Riddle</a>, <a href="https://github.com/DezSays"  target="_blank">Dezarea Bryan</a>, <a href="https://github.com/gm61091" target="_blank">Gary Merriman</a></p>
+            <p>Copyright &copy; 
+                <a href="https://github.com/jamescr757" target="_blank">James Riddle</a>
+                <a href="https://github.com/DezSays"  target="_blank">Dezarea Bryan</a> 
+                <a href="https://github.com/gm61091" target="_blank">Gary Merriman</a>
+            </p>
         </footer>
         </>
     )
