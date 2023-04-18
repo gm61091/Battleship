151062c5fcef1d162abe865a471693e717a70efd
Dezarea Bryan <dezareabryan@gmail.com>
2023-04-18 19:31:21 -0400

    Formatted files in server/author.

--- server/auth/passAuth.js
+++ server/auth/passAuth.js
@@ -1,4 +1,4 @@
-const passport = require("passport"); 
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
@@ -6,23 +6,24 @@ const bcrypt = require("bcrypt");
 const db = require("../models");
 const secretObj = require("../secrets");
 
-const localLogin = new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+const localLogin = new LocalStrategy(
+  { usernameField: "email" },
+  async (email, password, done) => {
     try {
-        const record = await db.User.findAll({ where: { email } });
-        if (record.length) {
-            bcrypt.compare(password, record[0].password, (error, match) => {
-                if (error) return done(error);
-                if (!match) return done(null, false);
-                return done(null, record[0]);
-            })
-        } else return done(null, false)
+      const record = await db.User.findAll({ where: { email } });
+      if (record.length) {
+        bcrypt.compare(password, record[0].password, (error, match) => {
+          if (error) return done(error);
+          if (!match) return done(null, false);
+          return done(null, record[0]);
+        });
+      } else return done(null, false);
     } catch (error) {
-        console.log(error.message);
-        return done(error);
+      console.log(error.message);
+      return done(error);
     }
-})
-
-
+  }
+);
 
 passport.use(localLogin);
 
@@ -31,42 +32,34 @@ passport.use(localLogin);
  * * check to see if token is valid
  */
 
- let jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-    secretOrKey: secretObj.secret, 
-}
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: secretObj.secret,
+};
 
-let jwtLogin = new JwtStrategy(jwtOptions, async (payload, done)=>{
+let jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+  try {
+    //check if user is in db
 
+    let userID = payload.sub;
 
-    try{
-        //check if user is in db
+    let user = await db.User.findByPk(userID); //{}
 
-        let userID = payload.sub;
+    //true - success
 
-        let user = await db.User.findByPk(userID); //{}
+    if (user) {
+      return done(null, user); //place the user object on req.user
 
-        //true - success
-
-        if(user){
-            return done(null, user)  //place the user object on req.user
-
-            //req.user = {id, email, password}
-        }
-        else{
-
-            //else - error
-            return done(null, false)
-
-        }
-
+      //req.user = {id, email, password}
+    } else {
+      //else - error
+      return done(null, false);
     }
-    catch(error){
-        //error reading db 
+  } catch (error) {
+    //error reading db
 
-        return done(error)
-    }
+    return done(error);
+  }
+});
 
-})
-
-passport.use(jwtLogin)
+passport.use(jwtLogin);
