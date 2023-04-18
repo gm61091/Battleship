a84e31b301a410cf3aedd31c40ab1c002a905cc8
Dezarea Bryan <dezareabryan@gmail.com>
2023-04-18 19:27:11 -0400

    Formatted files in root of Server directory.

--- server/app.js
+++ server/app.js
@@ -6,11 +6,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(require("./routes"))
+app.use(require("./routes"));
 
-db.sequelize.sync({ force: false })
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    })
-})
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
